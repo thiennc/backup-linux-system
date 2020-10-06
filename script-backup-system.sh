d00b1b7fd6d681ec5a3d54b4aabf09e244f5dc71
Thien Nguyen <thiennc@live.com>
2020-10-06 12:14:09 +0700

    Update script-backup-system.sh

--- script-backup-system.sh
+++ script-backup-system.sh
@@ -3,9 +3,11 @@ PROJECT_NAME="project1"
 PUBLIC_IP="123.123.123.123"
 BACKUP_PATH="/home/backup/"
 BACKUP_FOLDER="backup-system-"$PROJECT_NAME"-"$PUBLIC_IP"-$(date +"%Y%m%d")"
+ENCRYPT_KEY="sample-project-backup-key-20201006120928.pem"
 
 mkdir -p $BACKUP_PATH
 cd $BACKUP_PATH
+
 mkdir -p $BACKUP_FOLDER/etc
 cp -r /etc/nginx $BACKUP_FOLDER/etc
 cp -r /etc/apache2 $BACKUP_FOLDER/etc
@@ -22,5 +24,5 @@ cp -r /var/spool/cron $BACKUP_FOLDER/var/spool
 tar -czvf $BACKUP_FOLDER.tar.gz $BACKUP_FOLDER
 rm -rf $BACKUP_FOLDER
 
-openssl smime -encrypt -binary -aes-256-cbc -in $BACKUP_FOLDER.tar.gz -out $BACKUP_FOLDER.tar.gz.enc -outform DER keys/key-backup-$PROJECT_NAME.pem
+openssl smime -encrypt -binary -aes-256-cbc -in $BACKUP_FOLDER.tar.gz -out $BACKUP_FOLDER.tar.gz.enc -outform DER keys/$ENCRYPT_KEY
 rm $BACKUP_FOLDER.tar.gz
